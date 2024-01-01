6160381240085064ade909554cdee17a651ec509
LieStage <118051769+LieStage@users.noreply.github.com>
2024-01-01 15:33:54 +0530

    Update cmd.py

--- AniPlay/plugins/cmd.py
+++ AniPlay/plugins/cmd.py
@@ -1,20 +1,67 @@
+from pyrogram import Client
+from pyrogram import filters, enums
+from pyrogram.types import InlineKeyboardMarkup,InlineKeyboardButton
+from pyrogram.errors import UserNotParticipant
 from pyrogram.types import Message
 from pyrogram import filters
 from AniPlay import app
 from AniPlay.plugins.AnimeDex import AnimeDex
 from AniPlay.plugins.button import BTN
 from AniPlay.plugins.ErrorHandler import CMDErrorHandler
+from config import API_ID,TOKEN,API_HASH
+
+
+bot_token = os.environ.get("TOKEN", "")
+api_hash = os.environ.get("HASH", "") 
+api_id = os.environ.get("ID", "")
+OWNER_ID = os.environ.get("OWNER_ID", "")
+ADMIN_LIST = [int(ch) for ch in (os.environ.get("ADMIN_LIST", f"{OWNER_ID}")).split()]
+OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "FLIGHER")
+PERMANENT_GROUP = os.environ.get("PERMANENT_GROUP", "-1002045115155")
+GROUP_ID = [int(ch) for ch in (os.environ.get("GROUP_ID", f"{PERMANENT_GROUP}")).split()]
+UPDATES_CHANNEL = str(os.environ.get("UPDATES_CHANNEL", "trumbots"))
+app = Client("my_bot",api_id=api_id, api_hash=api_hash,bot_token=bot_token)  
 
 
 @app.on_message(filters.command(["start", "ping", "help", "alive"]))
 @CMDErrorHandler
-async def start(_, message: Message):
-    try:
-        await message.reply_photo("https://graph.org/file/d2d4bec6d5a46b27724af.jpg",caption=
-            "Bot Is Online...\n\nSearch Animes Using /search animename or /s animename"
-        )
-    except:
+async def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
+    if str(message.chat.id).startswith("-100") and message.chat.id not in GROUP_ID:
         return
+    elif message.chat.id not in GROUP_ID:
+        if UPDATES_CHANNEL != "None":
+            try:
+                user = await app.get_chat_member(UPDATES_CHANNEL, message.chat.id)
+                if user.status == enums.ChatMemberStatus.BANNED:
+                    await app.send_message(
+                        chat_id=message.chat.id,
+                        text=f"__Sorry, you are banned. Contact My [ Owner ](https://telegram.me/{OWNER_USERNAME})__",
+                        disable_web_page_preview=True
+                    )
+                    return
+            except UserNotParticipant:
+                 await app.send_message(
+                    chat_id=message.chat.id,
+                    text="<i>🔐 Join Channel To Use Me 🔐</i>",
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton("🔓 Join Now 🔓", url=f"https://t.me/{UPDATES_CHANNEL}")
+                            ]
+                        ]
+                    ),
+
+                )
+                 return
+            except Exception:
+                await app.send_message(
+                    chat_id=message.chat.id,
+                    text=f"<i>Something went wrong</i> <b> <a href='https://telegram.me/{OWNER_USERNAME}'>CLICK HERE FOR SUPPORT </a></b>",
+
+                    disable_web_page_preview=True)
+                return
+    await app.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**, i am Link Bypasser Bot, just send me any supported links and i will you get you results.\nCheckout /help to Read More__",
+                           reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Source Code 🌐", url="https://github.com/bipinkrish/Link-Bypasser-Bot")]]), reply_to_message_id=message.id)
 
 
 QUERY = "**Search Results:** `{}`"
